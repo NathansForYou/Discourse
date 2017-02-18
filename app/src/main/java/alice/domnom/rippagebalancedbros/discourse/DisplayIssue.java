0ad94f343850606f3ea3484d0fa9454ff1951642
Dominic Napoleon <dominicnapoleon@Dominics-MacBook-Pro.local>
2017-02-18 15:21:12 -0500

    Response view finished, adding cards

--- app/src/main/java/alice/domnom/rippagebalancedbros/discourse/DisplayIssue.java
+++ app/src/main/java/alice/domnom/rippagebalancedbros/discourse/DisplayIssue.java
@@ -3,6 +3,7 @@ package alice.domnom.rippagebalancedbros.discourse;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -18,6 +19,11 @@ public class DisplayIssue extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.issueDescription);
         textView.setText("Syria is ripping its prip. What would YOU do about Aleppo?");
 
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_issue);
+//        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_issue);
+    }
+
+    public void goToReflectionPrompt(View view) {
+        Intent intent = new Intent(this, ReflectionPrompt.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/alice/domnom/rippagebalancedbros/discourse/ReflectionPrompt.java
@@ -0,0 +1,19 @@
+package alice.domnom.rippagebalancedbros.discourse;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ReflectionPrompt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reflection_prompt);
+        Intent intent = getIntent();
+        TextView textView = (TextView) findViewById(R.id.reflectionQuestion);
+        textView.setText("What is something meaningful you heard from the other person?");
+    }
+}

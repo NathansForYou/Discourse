85f4f619cd273f5760505baf19c299063ae8aa44
Dominic Napoleon <dominicnapoleon@Dominics-MacBook-Pro.local>
2017-02-18 18:01:20 -0500

    beginning to add dynamic cards to reflections list

--- app/src/main/java/alice/domnom/rippagebalancedbros/discourse/IssueReflections.java
+++ app/src/main/java/alice/domnom/rippagebalancedbros/discourse/IssueReflections.java
@@ -2,6 +2,21 @@ package alice.domnom.rippagebalancedbros.discourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
 public class IssueReflections extends AppCompatActivity {
 
@@ -9,5 +24,48 @@ public class IssueReflections extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue_reflections);
+    }
+
+
+
+    CardView createCard(String message, int numLoves) {
+        CardView card = new CardView(getApplicationContext());
+
+        // Set the CardVi`ew layoutParams
+        LayoutParams params = new LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT
+        );
+        card.setLayoutParams(params);
+
+        // Set CardView corner radius
+        card.setRadius(9);
+
+        // Set cardView content padding
+        card.setContentPadding(15, 15, 15, 15);
+
+        // Set a background color for CardView
+        card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
+
+        // Set the CardView maximum elevation
+        card.setMaxCardElevation(15);
+
+        // Set CardView elevation
+        card.setCardElevation(9);
+
+        // Initialize a new TextView to put in CardView
+        TextView tv = new TextView(getApplicationContext());
+        tv.setLayoutParams(params);
+        tv.setText("CardView\nProgrammatically");
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+        tv.setTextColor(Color.RED);
+
+        // Put the TextView in CardView
+        card.addView(tv);
+
+        // Finally, add the CardView in root layout
+        //mRelativeLayout.addView(card);
+
+        return card;
     }
 }

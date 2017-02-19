14e61290a347b61823b89f6cf8a90372965b9cab
Dominic Napoleon <dominicnapoleon@Dominics-MacBook-Pro.local>
2017-02-18 23:02:18 -0500

    video with duplicate apk warnings :(

--- app/src/main/java/alice/domnom/rippagebalancedbros/discourse/MainActivity.java
+++ app/src/main/java/alice/domnom/rippagebalancedbros/discourse/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+//import com.twilio.video.quickstart;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override

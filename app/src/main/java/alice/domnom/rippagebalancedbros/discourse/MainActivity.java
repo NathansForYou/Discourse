683792606affa2593f3a3ee252fefee58ec23811
Nathan Smith <nathansforyou@gmail.com>
2017-02-18 22:03:08 -0500

    More firebase attempts, it's a lost cause

--- app/src/main/java/alice/domnom/rippagebalancedbros/discourse/MainActivity.java
+++ app/src/main/java/alice/domnom/rippagebalancedbros/discourse/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         // [START initialize_auth]
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
+
+        // Button btnLogIn = (Button) findViewById(R.id.button1);
 
         // [START auth_state_listener]
         mAuthListener = new FirebaseAuth.AuthStateListener() {
